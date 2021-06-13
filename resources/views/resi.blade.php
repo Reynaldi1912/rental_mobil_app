460f45b144197491784ce9881861c29b02925dae
= <=>
2021-06-13 19:29:51 +0700

    Create view Resi

---
+++ resources/views/resi.blade.php
@@ -0,0 +1,110 @@
+<html>
+    <head>
+        <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="/css/ruang-admin.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <h4 class="text-primary mt-5"><strong>Rincian Transaksi</strong></h4>
+            <hr style="border-color:blue">
+            <div class="row mt-5">
+                <div class="col-8 row text-center">
+                <div class="col-2"></div>
+                <div class="col">
+                    <div class="container border border-3 border-secondary p-2">
+                        <h4>Jumlah Pembayaran</h4>
+                        <h4><strong>Rp.200000</strong></h4>
+                    </div>
+                </div>
+                <div class="col-2"></div>
+                </div>
+                <div class="col-4">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr style="background-color:#DEFFFA">
+                                <th scope="col"><h4 class="text-primary"><strong>Detail Pembayaran</strong></h4></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div class="row">
+                                        <div class="col text-start">
+                                            <p>Metode Pembayaran</p>
+                                        </div>
+                                        <div class="col">
+                                            <p class="text-right">Bayar Ditempat</p>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="row">
+                                        <div class="col text-start">
+                                            <p>Harga Sewa /hari</p>
+                                        </div>
+                                        <div class="col">
+                                            <p class="text-right">Rp.100000</p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col text-start">
+                                            <p>Jumlah Hari Sewa</p>
+                                        </div>
+                                        <div class="col">
+                                            <p class="text-right">2 Hari</p>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="row">
+                                        <div class="col text-start">
+                                            <p>Total Pembayaran</p>
+                                        </div>
+                                        <div class="col">
+                                            <p class="text-right">Rp.200000</p>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div class="row border p-2" style="background-color:#DEFFFA">
+                <div class="col">
+                    Nomor Pesanan : 4564365362
+                </div>
+                <div class="col text-right">
+                    Dipesan pada tanggal 11 Juni 2021
+                </div>
+            </div>
+
+            <div class="row border border-2 mt-3 p-2">
+                <div class="col">  
+                    DESKRIPSI MOBIL
+                    <hr> 
+                    <h5><strong>Mobil Avanza</strong></h5>
+                    <p>Type Kendaraan : City Car</p>
+                    <p>Digunakan Tanggal 12 Juni 2021 dengan penyewaan mobil 2 hari</p>
+                </div>
+                <div class="col">  
+                    DESKRIPSI PENYEWA
+                    <hr> 
+                    <h5><strong>Dimasqi Allifudin Faiz</strong></h5>
+                    <p>NIK : 1263727637264</p>
+                    <p>Alamat : jl.Bojonergoro no 12 , Kabupaten Bojonegoro</p>
+                </div>
+            </div>
+            <div class="container-fluid pt-1" style="background-color:#DEFFFA">
+                <p class="text-center">RESI INI DISIMPAN DAN DIBERIKAN KE ADMIN SAAT PENGAMBILAN MOBIL <br> <strong>RESI INI BERLAKU : 12 Juni 2021</strong></p>
+            </div>
+
+        </div>
+    </body>
+</html>
