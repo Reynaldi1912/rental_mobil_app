2772f267f74a61e6899ff2a72924b5c8a4811af4
= <=>
2021-06-14 19:06:35 +0700

    update master view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -254,6 +254,15 @@
       modal.find('.modal-body #cat_id').val(cat_id);
     })
 
+    $('#sewa_sekarang').on('show.bs.modal',function(event){
+      var button = $(event.relatedTarget)
+
+      var cat_id = button.data('catid')
+      var modal = $(this)
+
+      modal.find('.modal-body #cat_id').val(cat_id);
+    })
+
     $('#editModal').on('show.bs.modal',function(event){
       var button = $(event.relatedTarget)
 
